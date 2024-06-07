6aa51d7ed215d058ada6610aa10d896baf7bde8d
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-07 10:20:49 +0500

    added the player to the center

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -15,21 +19,55 @@ class GameScreen extends StatelessWidget {
 class MyGame extends FlameGame {
   @override
   Color backgroundColor() {
-    return Colors.white30;
+    // TODO: implement backgroundColor
+    return Colors.blueAccent.withOpacity(0.3);
   }
 
   @override
   Future<void> onLoad() async {
-    // Load your game assets here
+    world.add(Player());
+  }
+}
+
+class Player extends PositionComponent with HasGameRef<MyGame>, DragCallbacks {
+ 
+ Offset? dragStart;
+ Offset? dragUpdate;
+  Offset? dragEnd;
+ 
+  @override
+  FutureOr<void> onLoad() {
+    return super.onLoad();
   }
 
   @override
   void update(double dt) {
-    // Update your game state here
+    super.update(dt);
   }
+
+  /*
+  
+  When i start dragging the player, the position of the player stays  the same but a line in the direction of the drag is drawn.
+  The straight line originates from the center of the player and extends to the point where the drag is released.
+  during the motion the line should be rendered on the screen.
+   */
+
+  @override
+  void onDragStart(DragStartEvent event) {
+    drag
+    super.onDragStart(event);
+  }
+
+
+
 
   @override
   void render(Canvas canvas) {
-    // Render your game here
+    super.render(canvas);
+
+    // drawing the player
+    canvas.drawCircle(position.toOffset(), 15, Paint()..color = Color.fromARGB(255, 193, 201, 236));
   }
 }
+
+
