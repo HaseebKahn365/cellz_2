f04731bd25959e171a57b894d3bc2b26a2abe65c
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-11 18:58:45 +0500

    working hard on the safeLines

--- test/Buisiness_Logic/aiFunction.dart
+++ test/Buisiness_Logic/aiFunction.dart
@@ -142,6 +142,8 @@ In case if the line is vertical:
       Line rightVert = Line(firstPoint: p4, secondPoint: p2);
 
       if (tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(rightVert.toString())) {
+        //also add the square to the tempFirstChainSquaresOwned list
+        tempFirstChainSquaresOwned.add(Square(topHoriz: topHoriz, bottomHoriz: line, leftVert: leftVert, rightVert: rightVert, isMine: false));
         return true;
       }
 
@@ -153,6 +155,8 @@ In case if the line is vertical:
       rightVert = Line(firstPoint: p2, secondPoint: p4);
 
       if (tempAllLinesDrawn.containsKey(bottomHoriz.toString()) && tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(rightVert.toString())) {
+        //also add the square to the tempFirstChainSquaresOwned list
+        tempFirstChainSquaresOwned.add(Square(topHoriz: line, bottomHoriz: bottomHoriz, leftVert: leftVert, rightVert: rightVert, isMine: false));
         return true;
       }
     } else {
@@ -167,6 +171,8 @@ In case if the line is vertical:
       Line bottomHoriz = Line(firstPoint: p4, secondPoint: p2);
 
       if (tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+        //also add the square to the tempFirstChainSquaresOwned list
+        tempFirstChainSquaresOwned.add(Square(topHoriz: topHoriz, bottomHoriz: bottomHoriz, leftVert: leftVert, rightVert: line, isMine: false));
         return true;
       }
 
@@ -178,12 +184,209 @@ In case if the line is vertical:
       bottomHoriz = Line(firstPoint: p2, secondPoint: p4);
 
       if (tempAllLinesDrawn.containsKey(rightVert.toString()) && tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+        //also add the square to the tempFirstChainSquaresOwned list
+        tempFirstChainSquaresOwned.add(Square(topHoriz: topHoriz, bottomHoriz: bottomHoriz, leftVert: line, rightVert: rightVert, isMine: false));
         return true;
       }
     }
     return false;
   }
+
+  //After calling the firstMaxChainFinder() method, we will have the firstMaxSquareChain list filled with the lines that can be drawn to create a chain of squares.
+  //now we need to implement the code to find the safeLines
+  //we need safelines to prevent the human player from creating a square and also do a trickshot in case if there are no safeLines available.
+  //A trickshot is move in which we skip the second last line of firstMaxSquareChain and draw the last line to make sure that human gets turn. which will force him to take two squares but he will have to draw an unsafe line which will give us a chance to create a square or a chain of square.
+  /*
+  Now we are gonna find a safe line: A safe line is a line in the game which can be drawn 
+  so that the apponent doesn’t get a chance to own a square. 
+  There are many safeLines in the beginning of the game but as the game progresses, 
+  the newLines will have a greater tendency to form a square and it becomes kinda’ hard to find a safeLine in the game. 
+  Therefore, the following rules will help us determine where in the game a safeLine might be present. 
+  
+  we will utilize the tempRemainingLines which now contains the lines without the first max square chain lines.
+  we are going to go through every line in this map and make sure that we don't give human a chance to create a square.
+  There are several scenarios in which a line can be considered as an unsafe line.
+  if any of the following conditions are met, then the line is considered as an unsafe line.
+
+ 
+  
+   */
+
+  /*
+
+        Altogether the naming for the lines should be as follows: 
+    •	for the top line in the Top right square the line name is TTR 
+    •	for the left line in the Top right square the line name is LTR 
+    •	for the bottom line in the Top right square the line name is BTR 
+    •	for the right line in the Top right square the line name is RTR 
+    
+    •	for the top line in the Top left square the line name is TTL 
+    •	for the left line in the Top left square the line name is LTL 
+    •	for the bottom line in the Top left square the line name is BTL 
+    •	for the right line in the Top left square the line name is RTL 
+    
+    The BR is not needed for the finding of safeLines
+    // •	for the top line in the Bottom right square the line name is TBR 
+    // •	for the left line in the Bottom right square the line name is LBR 
+    // •	for the bottom line in the Bottom right square the line name is BBR 
+    // •	for the right line in the Bottom right square the line name is RBR 
+    
+    •	for the top line in the Bottom left square the line name is TBL 
+    •	for the left line in the Bottom left square the line name is LBL 
+    •	for the bottom line in the Bottom left square the line name is BBL 
+    •	for the right line in the Bottom left square the line name is RBL 
+    */
+
+  /*
+        If the line that is being tested is a vertical line, then we need to check for the potential cases where line will allow for the creation of right square or left square.
+        we will create 4 more points two on each side of point 1 and point 2 of the current line.
+        two points on the left side and two points on the right side of the current line.
+
+        Scenario 1:
+        Risk of allowing the human to create a square on the left side if we draw this vertical line.
+        In this case, the current line is an RTL line.
+        if TTL and BTL exist then the line is unsafe.
+        if LTL and TTL exist then the line is unsafe.
+        if LTL and BTL exist then the line is unsafe.
+        in all of the above cases we will return false if any of the above conditions are met.
+
+        Scenario 2:
+        Risk of allowing the human to create a square on the right side if we draw this vertical line.
+        In this case, the current line is an LTR line.
+        if TTR and BTR exist then the line is unsafe.
+        if TTR and RTR exist then the line is unsafe.
+        if RTR and BTR exist then the line is unsafe.
+
+        Scenario 3 and 4 are for the horizontal lines. we will check for the potential cases where line will allow for the creation of top square or bottom square.
+        we should create 4 more points two on each side of point 1 and point 2 of the current line. two points above and two points below the current line.
+        Scenario 3:
+        Risk of allowing the human to create a square on the top side if we draw this horizontal line.
+        In this case, the current line is a BTL line.
+        if LTL and RTL exist then the line is unsafe.
+        if LTL and TTL exist then the line is unsafe.
+        if TTL and RTL exist then the line is unsafe.
+
+        Scenario 4:
+        Risk of allowing the human to create a square on the bottom side if we draw this horizontal line.
+        In this case, the current line is a TBL line.
+        if LBL and RBL exist then the line is unsafe.
+        if LBL and BBL exist then the line is unsafe.
+        if BBL and RBL exist then the line is unsafe.
+
+         */
+  bool checkSafeLine(Line line) {
+    if (line.direction == LineDirection.vert) {
+      Point p1 = line.firstPoint;
+      Point p2 = line.secondPoint;
+      // adding null safety
+      Point p3, p4;
+      Line topHoriz, bottomHoriz;
+      if (allPoints[p1.location - 1] != null && allPoints[p2.location - 1] != null) {
+        p3 = allPoints[p1.location - 1]!;
+        p4 = allPoints[p2.location - 1]!;
+
+        topHoriz = Line(firstPoint: p3, secondPoint: p1);
+        bottomHoriz = Line(firstPoint: p4, secondPoint: p2);
+
+        if (tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+          return false;
+        }
+
+        //creating leftVert and checking if leftVert and topHoriz are already drawn
+        Line leftVert = Line(firstPoint: p3, secondPoint: p4);
+        if (tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(topHoriz.toString())) {
+          return false;
+        }
+
+        //checking if leftVert and bottomHoriz are already drawn
+        if (tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+          return false;
+        }
+      }
+
+      if (allPoints[p1.location + 1] != null && allPoints[p2.location + 1] != null) {
+        p3 = allPoints[p1.location + 1]!;
+        p4 = allPoints[p2.location + 1]!;
+
+        topHoriz = Line(firstPoint: p1, secondPoint: p3);
+        bottomHoriz = Line(firstPoint: p2, secondPoint: p4);
+
+        if (tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+          return false;
+        }
+
+        //creating rightVert and checking if rightVert and topHoriz are already drawn
+        Line rightVert = Line(firstPoint: p3, secondPoint: p4);
+        if (tempAllLinesDrawn.containsKey(rightVert.toString()) && tempAllLinesDrawn.containsKey(topHoriz.toString())) {
+          return false;
+        }
+
+        //checking if rightVert and bottomHoriz are already drawn
+        if (tempAllLinesDrawn.containsKey(rightVert.toString()) && tempAllLinesDrawn.containsKey(bottomHoriz.toString())) {
+          return false;
+        }
+      }
+    } else {
+      //Line is horizontal
+      Point p1 = line.firstPoint;
+      Point p2 = line.secondPoint;
+      Point p3, p4;
+      Line leftVert, rightVert;
+
+      if (allPoints[p1.location - gameCanvas.xPoints] != null && allPoints[p2.location - gameCanvas.xPoints] != null) {
+        p3 = allPoints[p1.location - gameCanvas.xPoints]!;
+        p4 = allPoints[p2.location - gameCanvas.xPoints]!;
+
+        leftVert = Line(firstPoint: p3, secondPoint: p1);
+        rightVert = Line(firstPoint: p4, secondPoint: p2);
+
+        if (tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(rightVert.toString())) {
+          return false;
+        }
+
+        //creating topHoriz and checking if topHoriz and leftVert are already drawn
+        Line topHoriz = Line(firstPoint: p3, secondPoint: p4);
+        if (tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(leftVert.toString())) {
+          return false;
+        }
+
+        //checking if topHoriz and rightVert are already drawn
+        if (tempAllLinesDrawn.containsKey(topHoriz.toString()) && tempAllLinesDrawn.containsKey(rightVert.toString())) {
+          return false;
+        }
+      }
+
+      if (allPoints[p1.location + gameCanvas.xPoints] != null && allPoints[p2.location + gameCanvas.xPoints] != null) {
+        p3 = allPoints[p1.location + gameCanvas.xPoints]!;
+        p4 = allPoints[p2.location + gameCanvas.xPoints]!;
+
+        leftVert = Line(firstPoint: p1, secondPoint: p3);
+        rightVert = Line(firstPoint: p2, secondPoint: p4);
+
+        if (tempAllLinesDrawn.containsKey(leftVert.toString()) && tempAllLinesDrawn.containsKey(rightVert.toString())) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
