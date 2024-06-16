c178b3b1b73d44a4a71ca90f09d331479e74d1b5
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-16 09:41:10 +0500

    Update game_state.dart
    chain square formation capability added.
    turn can also be changed

--- lib/business_logic/game_state.dart
+++ lib/business_logic/game_state.dart
@@ -8,4 +8,16 @@ class GameState {
 
 // and Here is how we store the all points in the game
   static Map<int, Point> allPoints = {}; //key is the location aka the index of the point in the grid
+
+  //a static member to control the turns oof the player.
+  static bool myTurn = true; //if false then it is AI's turn
+
+  //To add some spice to the game, we record the chain of square formation which is identified if the turn doesn't change and player keeps making squares
+
+  static int chainCount = 0;
+
+  static void switchTurn() {
+    myTurn = !myTurn;
+    chainCount = 0;
+  }
 }
